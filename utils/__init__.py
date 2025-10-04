5654552ace18ef308fce406f4220aa6a07ade082
cellul4r <seriousme.com@gmail.com>
2025-10-04 12:09:44 +0700

    do Hands-on exercise2/ Task2

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,6 +5,7 @@ Initialize utils package
 from .llm_client import LLMClient, get_available_models, format_messages
 from .search_tools import WebSearchTool, format_search_results
 from .rag_system import SimpleRAGSystem, load_sample_documents, load_sample_documents_for_demo
+from .conversion_tools import CurrencyConversionTools
 
 __all__ = [
     'LLMClient',
